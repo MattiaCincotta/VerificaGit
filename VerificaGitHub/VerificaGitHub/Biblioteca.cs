ac227e867f9e968ed4693514bf4d48785691d7cf
Mattia Cincotta <MATTIA.CINCOTTA@local.ittcesena.it>
2024-10-21 08:33:30 +0200

    release 0.1 added Biblioteca class without functions

--- VerificaGitHub/VerificaGitHub/Biblioteca.cs
+++ VerificaGitHub/VerificaGitHub/Biblioteca.cs
@@ -8,5 +8,24 @@ namespace VerificaGitHub
 {
     internal class Biblioteca
     {
+        private string nome, indirizzo;
+        private int apertura, chiusura;
+        private List<Libro> libri;
+
+        public Biblioteca(string nome, string indirizzo, int apertura, int chiusura, List<Libro> libri)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.apertura = apertura;
+            this.chiusura = chiusura;
+            this.libri = libri;
+        }
+        public string Nome { get { return nome; } set { nome = value; } }
+        public string Indirizzo { get { return indirizzo; } set { indirizzo = value; } }
+        public int Apertura { get { return apertura; } set { apertura = value; } }
+        public int Chiusura { get { return chiusura; } set { chiusura = value; } }
+
+
+
     }
 }
