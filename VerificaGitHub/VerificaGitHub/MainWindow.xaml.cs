3af1060c077241be8106cd68d5415ac903515e3e
Mattia Cincotta <MATTIA.CINCOTTA@local.ittcesena.it>
2024-10-21 08:58:54 +0200

    release 0.1 added interface

--- VerificaGitHub/VerificaGitHub/MainWindow.xaml.cs
+++ VerificaGitHub/VerificaGitHub/MainWindow.xaml.cs
@@ -16,9 +16,46 @@ namespace VerificaGitHub
     /// </summary>
     public partial class MainWindow : Window
     {
+        Libro libro;
+        Biblioteca biblioteca;
         public MainWindow()
         {
             InitializeComponent();
+            libro = new Libro();
+            List<Libro> libri = new List<Libro>();
+            biblioteca = new Biblioteca();
+        }
+
+        private void btnAddBook_Click(object sender, RoutedEventArgs e)
+        {
+            biblioteca.AddBook(libro);
+        }
+
+        private void btnSearchByTitle_Click(object sender, RoutedEventArgs e)
+        {
+            biblioteca.SearchBookByTitle(txtSearchByTitle.Text);
+        }
+
+        private void btnSearchByAuthor_Click(object sender, RoutedEventArgs e)
+        {
+            biblioteca.SearchBookByAuthor(txtSearchByAuthor.Text);
+        }
+
+        private void btnBookNumber_Click(object sender, RoutedEventArgs e)
+        {
+            int bookNumber = biblioteca.BookNumber(libro);
+            txtBookNumber.Text = bookNumber.ToString();
+        }
+
+        private void btnReadingTime_Click(object sender, RoutedEventArgs e)
+        {
+            libro.ReadingTime();
+        }
+
+        private void btnToString_Click(object sender, RoutedEventArgs e)
+        {
+            string Stringa = libro.ToString();
+            txtBookNumber.Text = Stringa;
         }
     }
 }
