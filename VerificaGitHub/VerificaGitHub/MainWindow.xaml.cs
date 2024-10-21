739b3f658742d6b50197fbc4770f73a49a29680f
Mattia Cincotta <MATTIA.CINCOTTA@local.ittcesena.it>
2024-10-21 09:13:46 +0200

    release 0.2 fixed button functions

--- VerificaGitHub/VerificaGitHub/MainWindow.xaml.cs
+++ VerificaGitHub/VerificaGitHub/MainWindow.xaml.cs
@@ -21,29 +21,29 @@ namespace VerificaGitHub
         public MainWindow()
         {
             InitializeComponent();
-            libro = new Libro();
+            libro = new Libro(txtAuthor.Text, txtTitle.Text, txtEditor.Text,int.Parse(txtYear.Text), int.Parse(txtPageNumber.Text));
             List<Libro> libri = new List<Libro>();
-            biblioteca = new Biblioteca();
+            biblioteca = new Biblioteca(txtNome.Text, txtIndirizzo.Text, int.Parse(txtApertura.Text), int.Parse(txtChiusura.Text), libri);
         }
 
         private void btnAddBook_Click(object sender, RoutedEventArgs e)
         {
-            biblioteca.AddBook(libro);
+            biblioteca.addBook(libro);
         }
 
         private void btnSearchByTitle_Click(object sender, RoutedEventArgs e)
         {
-            biblioteca.SearchBookByTitle(txtSearchByTitle.Text);
+            biblioteca.searchBookByTitle(txtSearchByTitle.Text);
         }
 
         private void btnSearchByAuthor_Click(object sender, RoutedEventArgs e)
         {
-            biblioteca.SearchBookByAuthor(txtSearchByAuthor.Text);
+            biblioteca.searchBookByAuthor(txtSearchByAuthor.Text);
         }
 
         private void btnBookNumber_Click(object sender, RoutedEventArgs e)
         {
-            int bookNumber = biblioteca.BookNumber(libro);
+            int bookNumber = biblioteca.bookNumber();
             txtBookNumber.Text = bookNumber.ToString();
         }
 
@@ -55,7 +55,8 @@ namespace VerificaGitHub
         private void btnToString_Click(object sender, RoutedEventArgs e)
         {
             string Stringa = libro.ToString();
-            txtBookNumber.Text = Stringa;
+            txtToString.Text = Stringa;
         }
+
     }
 }
